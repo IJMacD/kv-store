268697fa0bb3117f29f61ad783cc9bb73951ae97
Iain MacDonald <IJMacD@gmail.com>
2024-10-08 22:36:43 +0800

    Enable DEBUG switch

---
+++ src/include/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+if (getenv("DEBUG")) {
+    define("DEBUG", true);
+}
--- src/index.php
+++ src/index.php
@@ -9,24 +9,27 @@ use KVStore\Controllers\WelcomeController;
 
 include "vendor/autoload.php";
 
-// Router::setController("/{bucket}/{key}", ObjectController::class);
-// Router::setController("/{bucket}", BucketController::class);
+include "include/bootstrap.php";
 
-Router::get("/", [WelcomeController::class, "index"]);
+// WelcomeController
+Router::get("/",                            [WelcomeController::class, "index"]);
 
-Router::post("/recover", [RecoverController::class, "recover"]);
+// RecoverController
+Router::post("/recover",                    [RecoverController::class, "recover"]);
 
-Router::post("/", [BucketController::class, "createBucket"]);
-Router::get("/{bucket}", [BucketController::class, "listObjects"]);
-Router::post("/{bucket}", [BucketController::class, "createObject"]);
+// BucketController
+Router::post("/",                           [BucketController::class, "createBucket"]);
+Router::get("/{bucket}",                    [BucketController::class, "listObjects"]);
+Router::post("/{bucket}",                   [BucketController::class, "createObject"]);
 
-Router::get("/{bucket}/{key}/meta", [ObjectController::class, "getMeta"]);
-Router::get("/{bucket}/{key}", [ObjectController::class, "get"]);
-Router::put("/{bucket}/{key}", [ObjectController::class, "createOrUpdate"]);
-Router::post("/{bucket}/{key}", [ObjectController::class, "createOrUpdate"]);
-Router::delete("/{bucket}/{key}", [ObjectController::class, "delete"]);
-Router::method("head", "/{bucket}/{key}", [ObjectController::class, "head"]);
-Router::method("patch", "/{bucket}/{key}", [ObjectController::class, "patch"]);
+// ObjectController
+Router::get("/{bucket}/{key}/meta",         [ObjectController::class, "getMeta"]);
+Router::get("/{bucket}/{key}",              [ObjectController::class, "get"]);
+Router::put("/{bucket}/{key}",              [ObjectController::class, "createOrUpdate"]);
+Router::post("/{bucket}/{key}",             [ObjectController::class, "createOrUpdate"]);
+Router::delete("/{bucket}/{key}",           [ObjectController::class, "delete"]);
+Router::method("head",  "/{bucket}/{key}",  [ObjectController::class, "head"]);
+Router::method("patch", "/{bucket}/{key}",  [ObjectController::class, "patch"]);
 
 try {
     Router::run();
